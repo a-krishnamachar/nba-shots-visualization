70285fd96409887283a1a8fa6825610b1b667691
Aditya Krishnamachar <adityakrishnamachar@gmail.com>
2021-11-04 11:44:38 -0500

    setup

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var courtSvg = d3.select("#court-area").append("svg")
+  .attr("viewBox", '0 0 800 800')
+
+var widthCourt = 800;
+var heightCourt = 800;
+
+d3.json("data/fairytales.json").then(function(data) {
+
+  console.log(data);
+
+  
+
+
+
+}
