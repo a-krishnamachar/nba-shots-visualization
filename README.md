af12d044e83d7ad3f4c7bd281b81d1324cb33ad2
Aditya Krishnamachar <adityakrishnamachar@gmail.com>
2021-11-18 12:09:10 -0600

    searchbar

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ We did use outside resources.
 Court background:
 https://stackoverflow.com/questions/28443229/using-d3-to-fill-an-svg-with-a-background-image
 
+Searchbar:
+https://www.w3schools.com/howto/howto_js_filter_lists.asp
+
 Add scroll feature / "tips" for viewer on page
 Responsive SVG
--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var heightCourt = 400;
 var court;
 
 allData = [];
+var playerArray = ["LeBron James", "Kyrie Irving", "Steph Curry", "Kevin Durant"];
 
 loadData();
 
@@ -26,5 +27,28 @@ function courtVis() {
     console.log("help");
     court = new CourtChart("court-area",shotData,regionData);
     distanceChart = new DistanceChart("distance-chart-area", shotData);
+
+}
+
+function searchFunc() {
+  //sample player names array -- this will have ALL past allstars from 5/10 years
+  var li;
+  var ul;
+  input = document.getElementById('playerSearch').value;
+  var node = document.createElement("li");
+  var textNode = document.createTextNode(input);
+  node.appendChild(textNode);
+  console.log(input);
+  // for (var i=0; playerArray.length; i++) {
+  if(playerArray.includes(input)) {
+      console.log('success');
+      // if(document.getElementById('playerList').contains(input)) {
+      //   console.log('already added');
+      // }
+      document.getElementById('playerList').appendChild(node);
+  }
+
+
+  // }
 
 }
